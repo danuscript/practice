50f665043703827e842fb114e01c8383b78ad36a
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:50:06 -0500

    Create Squared.java
    user input calculation practice

---
+++ University of Helsinki MOOC Java I/Part 2/Squared.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        int sum = (number * number);
+        System.out.println(sum);
+
+    }
+}
