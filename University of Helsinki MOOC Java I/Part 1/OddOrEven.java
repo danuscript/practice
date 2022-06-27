75009333c31836e04ba008a5ffe779fa894f8309
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:44:44 -0500

    Add OddOrEven.java
    boolean, integer, and double variable practice

---
+++ University of Helsinki MOOC Java I/Part 1/OddOrEven.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scan.nextLine());
+        int remainder = number % 2;
+        if (remainder == 0) {
+            System.out.println("Number " + number + " is even.");
+        } else {
+            System.out.println("Number " + number + " is odd.");
+        }
+    }
+}
