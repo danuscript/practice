190f64f52f34f5ad389185fadec15dde94bad624
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-06-27 13:30:01 -0500

    Add FromParameterToOne.java
    method with parameter practice

---
+++ University of Helsinki MOOC Java I/Part 2/FromParameterToOne.java
@@ -0,0 +1,23 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+        
+        printFromNumberToOne(21);
+
+
+    }
+    
+    public static void printFromNumberToOne(int number) {
+        
+        int i = number;
+        while (i > 0) {
+            
+            System.out.println(i);
+            i--; 
+            
+        }
+    }
+
+}
