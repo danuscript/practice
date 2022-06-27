f21bdbe1fe3066c30a2c436747767aca7ef343f0
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:26:47 -0500

    Add SimpleCalculator.java
    calculation with user input practice

---
+++ University of Helsinki MOOC Java I/Part 1/SimpleCalculator.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+        System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+        System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+        System.out.println(firstNumber + " / " + secondNumber + " = " + (1.0 * firstNumber / secondNumber));
+
+    }
+}
