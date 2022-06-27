2b51e8d331474f93df1c362df95e91d0aa92fbd1
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:57:44 -0500

    Add SumOfNumbers.java
    while loop with calculation practice

---
+++ University of Helsinki MOOC Java I/Part 2/SumOfNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        
+        while (true) {
+            
+            System.out.println("Give a number:");
+            int answer = Integer.valueOf(scanner.nextLine());
+            
+            if (answer == 0) {
+                
+                break;    
+            }
+            sum = sum + answer;
+            
+        }
+        
+        System.out.println("Sum of the numbers: " + sum);
+
+    }
+}
