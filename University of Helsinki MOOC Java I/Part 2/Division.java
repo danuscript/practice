3588707444bd719ec6c32908f98be442e6a6ab10
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-06-27 13:35:48 -0500

    Add Division.java
    method and calculation practice

---
+++ University of Helsinki MOOC Java I/Part 2/Division.java
@@ -0,0 +1,14 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+        
+        division(10, 2);
+    }
+
+    public static void division(int numerator, int denominator) {
+        
+        System.out.println(1.0 * numerator / denominator);
+    }
+}
