340b4818a32db430c07f958aa3a296a83242a180
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:16:49 -0500

    Update MessageThreeTimes.java

--- University of Helsinki MOOC Java I/Part 1/MessageThreeTimes.java
+++ University of Helsinki MOOC Java I/Part 1/MessageThreeTimes.java
@@ -7,7 +7,7 @@ public class MessageThreeTimes {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write a message:");
-        
+       
         String message = scanner.nextLine();
         
         System.out.println(message);
