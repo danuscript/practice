4677f1e8cc3416f23311de33d2f58dd3b851417d
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:46:20 -0500

    Add Same.java
    boolean and string variable practice

---
+++ University of Helsinki MOOC Java I/Part 1/Same.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Same {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the first string:");
+        String firstString = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String secondString = scan.nextLine();
+        
+        if (secondString.equals(firstString)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+    }
+}
