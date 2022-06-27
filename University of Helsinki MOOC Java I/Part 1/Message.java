98e60dcfb2f161ee657f3ac3bfa05e58fa02194c
danuscript <108024160+danuscript@users.noreply.github.com>
2022-06-27 09:16:21 -0500

    Update Message.java

--- University of Helsinki MOOC Java I/Part 1/Message.java
+++ University of Helsinki MOOC Java I/Part 1/Message.java
@@ -10,8 +10,7 @@ public class Message {
         
         String message = scanner.nextLine();
         
-        System.out.println(message);
-                
+        System.out.println(message);            
 
     }
 }
