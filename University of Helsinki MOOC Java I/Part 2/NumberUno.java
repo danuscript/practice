ccb5e2a79efef9cb08919f2ddfe56fcaf3e63bb3
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-06-27 14:08:38 -0500

    Add NumberUno.java
    method with value practice

---
+++ University of Helsinki MOOC Java I/Part 2/NumberUno.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+    }
+    
+    public static int numberUno() {
+        
+        return 1;
+    }
+
+}
