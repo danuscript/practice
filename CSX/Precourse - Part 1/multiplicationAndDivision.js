df5e5e6e1a3d54eb732176fd134b2ba6df73b49b
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:25:07 -0500

    Add multiplicationAndDivision.js

---
+++ CSX/Precourse - Part 1/multiplicationAndDivision.js
@@ -0,0 +1,7 @@
+let twentyTimesFour = 20 * 4
+let nineOverThree = 9 / 3
+let twoHundredTimesThree = 200.3 * 3
+
+console.log(twentyTimesFour);
+console.log(nineOverThree);
+console.log(twoHundredTimesThree);
