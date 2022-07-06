0c4b6908d888b900116c3ac21ad7c569677fe754
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-05 22:37:51 -0500

    Add theNameOfJavaScript.js
    if else statement practice

---
+++ JavaScript_Info/JavaScript Fundamentals/theNameOfJavaScript.js
@@ -0,0 +1,7 @@
+let officialName = prompt('What is the "official" name of Javascript?', "insert name");
+
+if (officialName == "EMCAScript") {
+    alert("Right!");
+} else {
+    alert("You don't know? EMCAScript!");
+}
