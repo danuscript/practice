c817749c36a66410d86a86006b9bda9ae3183368
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:09:18 -0500

    Add primitiveDataTypes.js

---
+++ CSX/Precourse - Part 1/primitiveDataTypes.js
@@ -0,0 +1,9 @@
+const name = "Danny"
+let age = 23
+const loveJavaScript = true
+let listOfComplaints = null
+
+console.log('My name is ' + name);
+console.log('I am ' + age + ' years old');
+console.log('It\'s ' + loveJavaScript + ', I love JavaScript');
+console.log('The list of things I hate about JavaScript is ' + listOfComplaints);
