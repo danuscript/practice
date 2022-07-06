d5e3872e493070319ae3c5f00c67f4cac81037e2
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:17:24 -0500

    Add bracketNotationForStrings.js

---
+++ CSX/Precourse - Part 1/bracketNotationForStrings.js
@@ -0,0 +1,10 @@
+const songTitle = "Space Oddity"
+const lyrics = "This is Ground Control to Major Tom / You’ve really made the grade"
+
+let firstLetter = songTitle[0];
+let seventhLetter = songTitle[6];
+let lastLetter = lyrics[lyrics.length - 1]
+
+console.log(firstLetter);
+console.log(seventhLetter);
+console.log(lastLetter);
