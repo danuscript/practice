b19086a4f196a532cfa05509a4509ebe3c3d3ac2
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:10:59 -0500

    Add strings.js

---
+++ CSX/Precourse - Part 1/strings.js
@@ -0,0 +1,7 @@
+let mySingleString = 'well hello there'
+let myDoubleString = "lovely weather we're having"
+let myBackString = `yes i do love breathing air`
+
+console.log(mySingleString);
+console.log(myDoubleString);
+console.log(myBackString);
