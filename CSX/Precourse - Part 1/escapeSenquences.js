81769dc3f61dfc7898213d4a472cdbe4b9ebbf24
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:12:07 -0500

    Add escapeSenquences.js

---
+++ CSX/Precourse - Part 1/escapeSenquences.js
@@ -0,0 +1,8 @@
+const path = "C:\\Users\\Win\\Files\\javascript.js";
+console.log(path);
+
+const heSaid = "He said, \"Hey Foo!\"";
+console.log(heSaid);
+
+const sheSaid = 'My name ain\'t Foo!';
+console.log(sheSaid);
