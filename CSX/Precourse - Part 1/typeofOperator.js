6d9c3fafbdb515497b60c43a4e4d8559906e1315
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:49:35 -0500

    Add typeofOperator.js

---
+++ CSX/Precourse - Part 1/typeofOperator.js
@@ -0,0 +1,12 @@
+const favMovie = "Star Wars: Episod IV";
+const timesSeen = 732;
+const goingToWatchItAgain = true; 
+
+let favMovieType = typeof(favMovie);
+let timesSeenType = typeof(timesSeen);
+let seeAgainType = typeof(goingToWatchItAgain);
+
+console.log(favMovieType);
+console.log(timesSeenType);
+console.log(seeAgainType);
+            
