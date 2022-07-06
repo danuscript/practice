7675c99669b94b581388adab3c549792ab5a8b44
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-06 17:15:44 -0500

    Add stringPropertiesAndMethods.js

---
+++ CSX/Precourse - Part 1/stringPropertiesAndMethods.js
@@ -0,0 +1,10 @@
+let hometown = "New York City";
+let favAlbum = "The Rise and Fall of Ziggy Stardust and the Spiders From Mars"
+
+let count = hometown.length;
+let upper = favAlbum.toUpperCase();
+let lower = favAlbum.toLowerCase();
+
+console.log(count);
+console.log(upper);
+console.log(lower);
