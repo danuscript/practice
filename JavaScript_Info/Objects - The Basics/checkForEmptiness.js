71569f443a9e6b1f58a7eb21ffbf226f8dd3bbe2
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-07 10:12:33 -0500

    Update checkForEmptiness.js

--- JavaScript_Info/Objects - The Basics/checkForEmptiness.js
+++ JavaScript_Info/Objects - The Basics/checkForEmptiness.js
@@ -1,14 +1,23 @@
-function isEmpty(object) {
-  for (let key in object) {
+function isEmpty(obj) {
+  for(let key in obj) {
     return false;
   }
   return true;
 }
 
 let schedule = {};
+schedule["8:30"] = "get up";
 
 console.log(isEmpty(schedule));
 
-schedule["8:30"] = "get up";
+delete schedule["8:30"];
+
+console.log(isEmpty(schedule));
+
+schedule.day = "WED";
+
+console.log(isEmpty(schedule));
+
+delete schedule.day;
 
 console.log(isEmpty(schedule));
