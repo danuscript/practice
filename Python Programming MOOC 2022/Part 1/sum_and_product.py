f12d81b930d4d431e38dfa9a50852e0681a78318
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-07 22:25:37 -0500

    Add sum_and_product.py

---
+++ Python Programming MOOC 2022/Part 1/sum_and_product.py
@@ -0,0 +1,4 @@
+first_number = int(input("Number 1: "))
+second_number = int(input("Number 2: "))
+print(f"The sum of the numbers: {first_number + second_number}")
+print(f"The product of the numbers: {first_number * second_number}")
