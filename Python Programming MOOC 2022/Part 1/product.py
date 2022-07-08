5b9e5f50f6796bd66bfbb29d13c907b5eca29890
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-07 22:23:15 -0500

    Add product.py

---
+++ Python Programming MOOC 2022/Part 1/product.py
@@ -0,0 +1,4 @@
+first_number = int(input("Please type in the first number: "))
+second_number = int(input("Please type in the second number: "))
+third_number = int(input("Please type in the third number: "))
+print(f"The product is {first_number * second_number * third_number}")
