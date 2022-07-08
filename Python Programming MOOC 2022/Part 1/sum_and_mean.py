7903574c901935494688a1bdef600e5a283ecb92
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-07 22:30:05 -0500

    Add sum_and_mean.py

---
+++ Python Programming MOOC 2022/Part 1/sum_and_mean.py
@@ -0,0 +1,7 @@
+first_number = int(input("Number 1: "))
+second_number = int(input("Number 2: "))
+third_number = int(input("Number 3: "))
+fourth_number = int(input("Number 4: "))
+sum = first_number + second_number + third_number + fourth_number
+mean = sum / 4
+print(f"The sum of the numbers is {sum} and the mean is {mean}")
