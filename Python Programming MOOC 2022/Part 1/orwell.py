21f72349a498dbf572876e4f3698afcbcd9fc7f3
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-08 20:11:15 -0500

    Update orwell.py

--- Python Programming MOOC 2022/Part 1/orwell.py
+++ Python Programming MOOC 2022/Part 1/orwell.py
@@ -1,6 +1,3 @@
-students = int(input("How many students on the course? "))
-group_size = int(input("Desired group size? "))
-groups = students // group_size
-if students % group_size != 0:
-    groups += 1
-print(f"Number of groups formed: {groups}")
+year = int(input("Please type in a number: "))
+if year == 1984:
+    print("Orwell")
