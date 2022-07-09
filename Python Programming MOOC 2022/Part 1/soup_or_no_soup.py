f6d1c5b52b4bed2b161d28a1c9548160d0d02993
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-08 20:17:56 -0500

    Update soup_or_no_soup.py

--- Python Programming MOOC 2022/Part 1/soup_or_no_soup.py
+++ Python Programming MOOC 2022/Part 1/soup_or_no_soup.py
@@ -1,3 +1,7 @@
-year = int(input("Please type in a number: "))
-if year == 1984:
-    print("Orwell")
+name = input("Please tell me your name: ")
+
+if name != "Jerry":
+    soup = float(input("How many portions of soup? "))
+    print(f"The total cost is {soup * 5.90}")
+
+print("Next please!")
