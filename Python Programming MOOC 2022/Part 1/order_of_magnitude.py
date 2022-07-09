9c038b44dc20ae7ed35a04184540d39d7a1e4d2f
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-08 20:21:16 -0500

    Add order_of_magnitude.py

---
+++ Python Programming MOOC 2022/Part 1/order_of_magnitude.py
@@ -0,0 +1,12 @@
+number = int(input("Please type in a number: "))
+
+if number < 1000:
+    print("This number is smaller than 1000")
+
+if number < 100:
+    print("This number is smaller than 100")
+
+if number < 10:
+    print("This number is smaller than 10")
+
+print("Thank you!")
