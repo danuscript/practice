f6c0239829134e29aba28d13d804b69bd026816c
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-11 17:46:02 -0500

    Add iLoveEvenNumbers.js

---
+++ CSX/Functions and Execution Context/iLoveEvenNumbers.js
@@ -0,0 +1,10 @@
+function iLoveEvenNumbers(num) {
+  if (num % 2 == 0) {
+    return "Oh Yeah Evens!";
+  } else {
+    return "I am too normal for odd numbers"
+  }
+}
+
+console.log(iLoveEvenNumbers(42)); 
+console.log(iLoveEvenNumbers(17)); 
