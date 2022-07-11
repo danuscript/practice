6dbdf9d464e84ac7a516040b55e570228bb513bf
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-11 16:38:19 -0500

    Add lastLetter.js

---
+++ CSX/Functions and Execution Context/lastLetter.js
@@ -0,0 +1,8 @@
+function lastLetter(word) {
+  return word[word.length - 1]
+}
+
+console.log(lastLetter("hello"));
+console.log(lastLetter("goodbye!"));
+console.log(lastLetter("ZeltoiD"));
+console.log(lastLetter("I love Javascript"));
