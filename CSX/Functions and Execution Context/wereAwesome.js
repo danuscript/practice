fe9c7f2a02e9a99ded2143d9635d14324ddd2c48
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-11 16:38:40 -0500

    Update wereAwesome.js

--- CSX/Functions and Execution Context/wereAwesome.js
+++ CSX/Functions and Execution Context/wereAwesome.js
@@ -2,4 +2,8 @@ function wereAwesome(nameA, nameB) {
   return `${nameB} and ${nameA} are awesome!`;
 }
 
-console.log(wereAwesome("Danny", "John"));
+
+console.log(wereAwesome("Dave", "Will"));
+console.log(wereAwesome("Victoria", "Jenny"));
+console.log(wereAwesome("Chris", "Jac"));
+console.log(wereAwesome("Phillip", "Skyler"));
