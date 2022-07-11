07fdfbb9dcbe3deeb36ec739acd278485224eff2
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-11 16:41:47 -0500

    Add passingArguments.js

---
+++ CSX/Functions and Execution Context/passingArguments.js
@@ -0,0 +1,16 @@
+const x = 3;
+
+function isX1(num) {
+    const x = 5;
+    return num === x;
+}
+
+function isX2(num) {
+    return num === x;
+}
+
+const one = isX1(5);
+const two = isX2(x);
+
+console.log(one); 
+console.log(two);
