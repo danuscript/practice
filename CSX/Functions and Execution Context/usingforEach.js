846e85d299fc1e758feaa7385a497032cba05c85
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-12 17:47:06 -0500

    Add usingforEach.js

---
+++ CSX/Functions and Execution Context/usingforEach.js
@@ -0,0 +1,16 @@
+function droids(arr) {
+  let result = '';
+
+  result = "These are not the droids you're looking for."
+
+  arr.forEach(element => {
+    if (element == "Droids") result = "Found Droids!"
+  });
+
+  return result;
+}
+ 
+const starWars = ["Luke", "Finn", "Rey", "Kylo", "Droids"] 
+const thrones = ["Jon", "Danny", "Tyrion", "The Mountain", "Cersei"] 
+console.log(droids(starWars))
+console.log(droids(thrones))
