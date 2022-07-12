b694da5f11471aa882e59fceaf8eb0611c2badb4
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-11 19:28:12 -0500

    Add shuffleAnArray.js

---
+++ JavaScript_Info/Data Types/shuffleAnArray.js
@@ -0,0 +1,11 @@
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+}
+
+let arr = [1, 2, 3];
+
+shuffle(arr);
+console.log(arr);
