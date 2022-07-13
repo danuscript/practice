4b64a3ae745973830474ae7b4bfc0b5cb2609a6c
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-13 14:54:31 -0500

    Add whileLoopsCounters.js

---
+++ CSX/Functions and Execution Context/whileLoopsCounters.js
@@ -0,0 +1,21 @@
+function imAboutToExplodeWithExcitement(n) {
+    while (n > 0) {
+        
+        
+        if (n == 5) {
+            console.log("Oh wow, I can't handle the anticipation!");
+            n--;
+        } else if (n == 3) {
+            console.log("I'm about to explode with excitement!");
+            n--;
+        } else {
+            console.log(n);
+            n--
+        }
+        
+
+    }
+    console.log("That was kind of a let down")
+}
+
+imAboutToExplodeWithExcitement(10);  
