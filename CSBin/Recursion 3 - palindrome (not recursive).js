76a2af131139867a0f846c61b3df680ec5e666ee
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-14 08:06:42 -0500

    Update Recursion 3 - palindrome (not recursive).js

--- CSBin/Recursion 3 - palindrome (not recursive).js
+++ CSBin/Recursion 3 - palindrome (not recursive).js
@@ -1,7 +1,1 @@
-function isPalindrome(str) {
-    if (str.toLowerCase() == str.toLowerCase().split("").reverse().join("")) return true;
-    return false;
-}
-
-console.log(isPalindrome("Hello"))
-console.log(isPalindrome("Racecar"))
+let isPalindrome = (s) => (s.toLowerCase() == s.toLowerCase().split("").reverse().join(""));
