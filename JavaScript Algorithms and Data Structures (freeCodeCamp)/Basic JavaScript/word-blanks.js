6c9906f0edf9e67316c250ac00b8050280f01327
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-14 11:39:38 -0500

    Add word-blanks.js

---
+++ JavaScript Algorithms and Data Structures (freeCodeCamp)/Basic JavaScript/word-blanks.js
@@ -0,0 +1,7 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+
+const wordBlanks = `The ${myNoun} was pretty ${myAdjective}, but it ${myVerb} suprisingly ${myAdverb}!`; 
