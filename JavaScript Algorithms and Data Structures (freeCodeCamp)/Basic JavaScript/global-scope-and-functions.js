1f250c0be289e82af39275622e3692cb219d7423
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-14 15:21:20 -0500

    Add global-scope-and-functions.js

---
+++ JavaScript Algorithms and Data Structures (freeCodeCamp)/Basic JavaScript/global-scope-and-functions.js
@@ -0,0 +1,20 @@
+let myGlobal = 10;
+
+
+function fun1() {
+
+oopsGlobal = 5;
+
+}
+
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
