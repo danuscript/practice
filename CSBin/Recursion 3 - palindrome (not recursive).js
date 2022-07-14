8ddd29d7884c960faabbb055f62c65ebff347063
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-14 08:35:39 -0500

    Update Recursion 3 - palindrome (not recursive).js

--- CSBin/Recursion 3 - palindrome (not recursive).js
+++ CSBin/Recursion 3 - palindrome (not recursive).js
@@ -1,1 +1,8 @@
-let isPalindrome = (s) => (s.toLowerCase() == s.toLowerCase().split("").reverse().join(""));
+function palindrome(string) {
+	return (string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "") == 
+          string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "").split("").reverse().join(""));
+}
+
+console.log(palindrome("Anne, I vote more cars race Rome-to-Vienna"));
+console.log(palindrome("llama mall"));
+console.log(palindrome("jmoney"));
