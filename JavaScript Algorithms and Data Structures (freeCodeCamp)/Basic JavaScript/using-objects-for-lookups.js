4be814f56c36776de1338908d6178ffa995fe2b9
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-15 10:19:43 -0500

    Add using-objects-for-lookups.js

---
+++ JavaScript Algorithms and Data Structures (freeCodeCamp)/Basic JavaScript/using-objects-for-lookups.js
@@ -0,0 +1,17 @@
+function phoneticLookup(val) {
+  let result = "";
+
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  }
+
+  result = lookup[val];
+  return result;
+}
+
+phoneticLookup("charlie");
