f29e344125434b45013ba18f463bc21e4b872722
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-15 10:32:46 -0500

    Add manipulating-complex-objects.js

---
+++ JavaScript Algorithms and Data Structures (freeCodeCamp)/Basic JavaScript/manipulating-complex-objects.js
@@ -0,0 +1,26 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic.push(
+  {
+    "artist": "Sade",
+    "title": "The Best of Sade",
+    "release_year": 1994,
+    "formats": [
+      "CD",
+      "LP"
+    ],
+    "gold": true,
+  }
+);
