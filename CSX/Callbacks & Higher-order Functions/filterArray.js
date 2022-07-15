51cde34fc23a82ed5e9d9fe4dbcd9daeda8cd897
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-14 20:26:46 -0500

    Add filterArray.js

---
+++ CSX/Callbacks & Higher-order Functions/filterArray.js
@@ -0,0 +1,18 @@
+function filterArray(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i += 1) {
+      if (callback(array[i])) newArray.push(array[i]);
+    }
+    return newArray;
+  }
+  const arrOfNums = [1, 2, 3, 4, 5];
+  
+  function func1(num) {
+    return (num % 2 == 0);
+  }
+  function func2(num) {
+    return (num % 2 != 0);
+  }
+  
+ console.log(filterArray(arrOfNums, func1));
+ console.log(filterArray(arrOfNums, func2));
