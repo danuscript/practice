beaafe130b762319f971fdac366486294f9b7fc8
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-15 08:51:39 -0500

    Add replacing-if-else-chains-with-switch.js

---
+++ JavaScript Algorithms and Data Structures (freeCodeCamp)/Basic JavaScript/replacing-if-else-chains-with-switch.js
@@ -0,0 +1,36 @@
+function chainToSwitch(val) {
+  let answer = "";
+
+  // if (val === "bob") {
+  //   answer = "Marley";
+  // } else if (val === 42) {
+  //   answer = "The Answer";
+  // } else if (val === 1) {
+  //   answer = "There is no #1";
+  // } else if (val === 99) {
+  //   answer = "Missed me by this much!";
+  // } else if (val === 7) {
+  //   answer = "Ate Nine";
+  // }
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+
+chainToSwitch(7);
