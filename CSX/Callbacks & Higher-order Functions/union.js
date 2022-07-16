5d0dc08f132b11b5a1f3e059044828363a68d8e8
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-16 09:02:59 -0500

    Update union.js

--- CSX/Callbacks & Higher-order Functions/union.js
+++ CSX/Callbacks & Higher-order Functions/union.js
@@ -9,18 +9,18 @@
     // return accumulator array that is updated with callbackFn
         //  merge accumulator and currentValue with spread operator and return new set from that array
 
-        const union = (arrs) => arrs.reduce((a, b) => [...new Set([...a, ...b])]);
+const union = (arrs) => arrs.reduce((a, b) => [...new Set([...a, ...b])]);
 
-        //approach 2:
-        //create a function "union" that returns an array
-            //array is comprised of new set from flattened input array
-                //new set is converted to array with spread operator
-                //array is flattened with .flat()
+//approach 2:
+//create a function "union" that returns an array
+    //array is comprised of new set from flattened input array
+        //new set is converted to array with spread operator
+        //array is flattened with .flat()
+
+const union2 = (arrs) => [...new Set(arrs.flat())];
         
-        const union2 = (arrs) => [...new Set(arrs.flat())];
-        
-        const arr1 = [5, 10, 15];
-        const arr2 = [15, 88, 1, 5, 7];
-        const arr3 = [100, 15, 10, 1, 5];
-        console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
-        console.log(union2([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
+const arr1 = [5, 10, 15];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [100, 15, 10, 1, 5];
+console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
+console.log(union2([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
