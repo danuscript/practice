51001a1d878c983de1c5a47aa01fb199b14ad013
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-18 11:04:39 -0500

    Add createFunction.js

---
+++ CSX/Closure, Scope & Execution Context/createFunction.js
@@ -0,0 +1,8 @@
+const num = 3;
+function multiplyBy2(inputNumber) {
+    const result = inputNumber * 2;
+    return result;
+}
+
+const output = multiplyBy2(num);
+const newOutput = multiplyBy2(10);
