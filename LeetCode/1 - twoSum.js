a65018844b43860fe02721ec7f69f3801031a9b3
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-18 22:28:12 -0500

    Update 1 - twoSum.js

--- LeetCode/1 - twoSum.js
+++ LeetCode/1 - twoSum.js
@@ -1,19 +1,27 @@
-var twoSum = function(nums, target) {
-    let localSum = 0;
-    let indices = [];
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = (i + 1); j < nums.length; j++) {
-            localSum = nums[i] + nums[j];
-            if (localSum == target) {
-                indices.push(i);
-                indices.push(j);
-                return indices;
-            }
+function twoSum(arr, tar) {
+
+    // declare a new hash map
+    let obj = {}
+
+    //iterate through array
+    for (let i = 0; i < arr.length; i++) {
+
+        //check map for correct answer: (target - current value)
+        if (obj.hasOwnProperty([tar - arr[i]])) {
+
+            //if it exists, return an array with the indices of current value and correct answer:
+            return [i, obj[tar - arr[i]]];
+
+        // if it doesn't exist
+        } else {
+
+            // push current value and its index into hash map
+            obj[arr[i]] = i;
         }
     }
-    
-};
 
+    // if no solution is found, return null
+    return null;
+}
 
-console.log(twoSum([2, 7, 9, 11], 9));
-
+console.log(twoSum([2, 7, 11, 15], 9))
