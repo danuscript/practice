acb2fa3c60dd590ccfd6454aa28c5dc0fac06b6f
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-20 07:54:44 -0500

    Update 118 - pascalsTriangle.js

--- LeetCode/118 - pascalsTriangle.js
+++ LeetCode/118 - pascalsTriangle.js
@@ -45,47 +45,83 @@
 function generate(numRows) {
     
 
-    // base cases:
-    if (numRows == 1) return [[1]];
-    if (numRows == 2) return [[1], [1, 1]];
+  // base cases:
+  if (numRows == 1) return [[1]];
+  if (numRows == 2) return [[1], [1, 1]];
 
-    // starting array
-    let pascalArrs = [[1], [1, 1]];
+  // define our starting array that gets returned at the end
+  let pascalArrs = [[1], [1, 1]];
 
-    for (let i = 2; i < numRows; i++) {
-        let newArr = [1, i];
+  // loop the following operation until we have the desired amount of row arrays
+  for (let i = 2; i < numRows; i++) {
+      
+    // define the new array we're going to add every time we loop
+    let newArr = [1, i];
 
-        // add the next elements of array
-        if (i >= 4) {
-            if (i % 2 == 0) {
-                for (let j = 1; j < (i/2); j++) {
-                    newArr.push(pascalArrs[pascalArrs.length -1][j] + pascalArrs[pascalArrs.length - 1][j + 1])
-                }
-            } else if (i % 2 != 0) {
-                for (let j = 1; j < ((i - 1)/2); j++) {
-                    newArr.push(pascalArrs[pascalArrs.length -1][j] + pascalArrs[pascalArrs.length - 1][j + 1])
-                }
-            }
-        }
-        
-        // mirror the array based off of its odd/evenness
-        if (i % 2 == 0) {
-            for (let j = ((i/2) - 1); j >= 0; j--) {
-                newArr.push(newArr[j])
-            }
-        } else if (i % 2 != 0) {
-            for (let j = (((i + 1)/2) - 1); j >= 0; j--) {
-                newArr.push(newArr[j])
-            }
-        }
+      // add the next elements into this new array
+      // if the current row number is 4 or higher, then
+      if (i >= 4) {
 
-        // push this new array into pascalized arrays
-        pascalArrs.push(newArr);
-    }
+          // if the row number is even, then:
+          if (i % 2 == 0) {
 
-    // return pascalized arrays
-    return pascalArrs;
+              // divide the row number by half
+              // repeat this operation from one until half the row number:
+              for (let j = 1; j < (i/2); j++) {
+
+                  // push the sum of the two elements at index j in the array at the last index of the starting array
+                  newArr.push(pascalArrs[pascalArrs.length -1][j] + pascalArrs[pascalArrs.length - 1][j + 1])
+              }
+
+          // else if the row number is odd, then:  
+          } else if (i % 2 != 0) {
+
+              // subtract one from the row number, then divide it by half
+              // repeat this operation from one until half the row number:
+              for (let j = 1; j < ((i - 1)/2); j++) {
+
+                  // push the sum of the two elements at index j in the array at the last index of the starting array
+                  newArr.push(pascalArrs[pascalArrs.length -1][j] + pascalArrs[pascalArrs.length - 1][j + 1])
+              }
+          }
+      }
+      
+      // next, mirror the array based off of its odd/evenness:
+      // if the row number is odd:
+      if (i % 2 == 0) {
+
+          // divide the row number by half, then subtract one
+          // repeat this operation from 0 until that divided row number
+          for (let j = ((i/2) - 1); j >= 0; j--) {
+
+              //push the element at index j in the new array back into itself at the end
+              newArr.push(newArr[j])
+          }
+
+      // else if the row number is even:
+      } else if (i % 2 != 0) {
+
+          // divide the row number by half
+          for (let j = (((i + 1)/2) - 1); j >= 0; j--) {
+
+              //push the element at index j in the new array back into itself at the end
+              newArr.push(newArr[j])
+          }
+      }
+
+      // next, push this entire new array into final array of arrays
+      pascalArrs.push(newArr);
+
+      // repeat this for next row number in the loop on line 19
+  }
+
+  // return final array of arrays
+  return pascalArrs;
 }
+
+console.log(generate(6))
+
+// time to write: 1 hour and 41 minutes
 
 console.log(generate(5))
 
