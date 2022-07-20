c4466fd5dd221bbdd353a8a44de43fe918a93f99
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-20 17:30:35 -0500

    Update 1346 - checkIfNAndItsDoubleExist.js

--- LeetCode/1346 - checkIfNAndItsDoubleExist.js
+++ LeetCode/1346 - checkIfNAndItsDoubleExist.js
@@ -1,14 +1,43 @@
-var checkIfExist = function(arr) {
-    let obj = {};
-    for (let i = 0; i < arr.length; i++) {
-        comp = (arr[i] * 2)
-        comp2 = (arr[i] / 2)
-        if (obj.hasOwnProperty(comp) || obj.hasOwnProperty(comp2)) {
-            return true; 
-        } else {
-            obj[arr[i]] = i;
-        }
-    
+// CHECK IF N AND ITS DOUBLE EXIST
+
+// expected input: array
+// expected output: boolean
+
+// approach: hash table
+
+// 1. iterate through each number
+// 2. check if its two complements exist in hash table: number * 2 or number / 2
+//   2A. if so, return true
+//   2B. if not, store number in hash table
+// 3. if we reach the end, return false
+
+
+
+var checkIfExist = function (arr) {
+
+  // use set to define new hash table
+  let hash = new Set;
+
+  // iterate through array of numbers
+  for (let i = 0; i < arr.length; i++) {
+
+    // define complements
+    comp = (arr[i] * 2)
+    comp2 = (arr[i] / 2)
+
+    // if either complement has been seen before
+    if (hash.has(comp) || hash.has(comp2)) {
+      
+      // return true
+      return true;
+
+    } else {
+
+      // otherwise, store this number in hash table
+      hash.add(arr[i]);
     }
-    return false;
+  }
+
+  // if the loop ends without any complements, return false
+  return false;
 };
