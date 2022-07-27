0fd888fb0e6db4f58f7c92db2c0fac6949bfea6d
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-07-27 09:12:18 -0500

    Update flattenRecursively.js

--- CSBin/flattenRecursively.js
+++ CSBin/flattenRecursively.js
@@ -1,11 +1,10 @@
 // declare a function that takes an array as a paramenter and stores a counter in the default parameters
 function flattenRecursively(arr, counter = 0) {
-    
+
     // base case:
     // if there is no array element at index [counter], return the array
     // this will only trigger when counter reaches the end of the array
     if (arr[counter] === undefined) return arr
-
 
     // recursive case 1: 
     // if the element at index [counter] is not an array,
@@ -19,18 +18,34 @@ function flattenRecursively(arr, counter = 0) {
     // recursive case 2:
     // if the element at index counter IS an array
 
-    // concatenate the flat portion of the array
-    let firstHalf = arr.slice(0, counter);
+    // if it's not the first element
+    if (counter > 0) {
 
-    // with the elements of the new array
-    let arrNew = firstHalf.concat(arr[counter]);
+        // concatenate the flat portion of the array
+        let firstHalf = arr.slice(0, counter);
+
+        // with the elements of the new array
+        let arrNew = firstHalf.concat(arr[counter]);
+
+        // recursively call the function, WITHOUT moving forward
+        // this will check this new array at the current element for an additional level of nesting
+        // the counter will only move on after it unpacks all nested arrays
+        return flattenRecursively(arrNew, counter)
+    } 
+
+    // if it IS the first element, 
+        
+    // concatenate the contents of the first element
+    let firstHalf = arr[0];
+
+    // with the remaining elements of the array
+    let arrNew = firstHalf.concat(arr.slice(1))
 
     // recursively call the function, WITHOUT moving forward
-    // this will check this new array at the current element for nested arrays
+    // this will check this new array at the current element for an additional level of nesting
     // the counter will only move on after it unpacks all nested arrays
     return flattenRecursively(arrNew, counter)
-
 }
 
-console.log(flattenRecursively([1, [2, 3, [4]]])); //-> [1, 2, 3, 4]
+console.log(flattenRecursively([[1], [2, 3, [4]]])); //-> [1, 2, 3, 4]
 console.log(flattenRecursively([1, {}, [3, [[4]]]])); //-> [1, {}, 3, 4]
