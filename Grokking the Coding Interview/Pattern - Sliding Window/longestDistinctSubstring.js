ce3e9307c48c9f2cd0496f96f23cc9b329d8522f
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-29 14:13:00 -0500

    Update longestDistinctSubstring.js

--- Grokking the Coding Interview/Pattern - Sliding Window/longestDistinctSubstring.js
+++ Grokking the Coding Interview/Pattern - Sliding Window/longestDistinctSubstring.js
@@ -4,15 +4,15 @@ const longestDistinctSubstring = (str) => {
   let windowStart = 0;
   for (let windowsEnd = 0; windowsEnd < str.length; windowsEnd += 1) {
     if (str[windowsEnd] in indexMap) {
-      windowStart = indexMap[str[windowsEnd]] + 1;
-      delete indexMap[str[windowsEnd]];
+      windowStart = Math.max(windowStart, indexMap[str[windowsEnd]] + 1);
     }
     indexMap[str[windowsEnd]] = windowsEnd;
-    maxLength = Math.max(maxLength, windowsEnd - windowStart + 1)
+    maxLength = Math.max(maxLength, windowsEnd - windowStart + 1);
   }
   return (maxLength === -Infinity) ? 0 : maxLength;
 };
 
+console.log(longestDistinctSubstring('abba')); // -> 2
 console.log(longestDistinctSubstring('aabccbb')); // -> 3
 console.log(longestDistinctSubstring('abbbb')); // -> 2
 console.log(longestDistinctSubstring('abccde')); // -> 3
