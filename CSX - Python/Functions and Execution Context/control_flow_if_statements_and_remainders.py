48fcced66f8f2f322ad6b07d0ff35bd13f362ceb
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 15:10:34 -0500

    Add control_flow_if_statements_and_remainders.py

---
+++ CSX - Python/Functions and Execution Context/control_flow_if_statements_and_remainders.py
@@ -0,0 +1,7 @@
+def i_love_even_numbers(num):
+    if num % 2 == 0: 
+        return 'Oh yeah Evens!'
+    return 'I am too normal for even numbers'
+
+print(i_love_even_numbers(42))
+print(i_love_even_numbers(17))
