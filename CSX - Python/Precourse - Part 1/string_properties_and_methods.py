db15de58d07cd9a3dbcdd479f27a95df64c85e9d
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 10:05:20 -0500

    Add string_properties_and_methods.py

---
+++ CSX - Python/Precourse - Part 1/string_properties_and_methods.py
@@ -0,0 +1,10 @@
+hometown = 'New York City'
+favAlbum = 'The Rise and Fall of Ziggy Stardust and the Spiders From Mars'
+
+count = len(hometown)
+upper = favAlbum.upper()
+lower = favAlbum.lower()
+
+print(count)
+print(upper)
+print(lower)
