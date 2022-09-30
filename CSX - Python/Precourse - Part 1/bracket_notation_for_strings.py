63cc6f0b63d8820ee600cc5b89a5af6e059e4e8b
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 10:20:19 -0500

    Add bracket_notation_for_strings.py

---
+++ CSX - Python/Precourse - Part 1/bracket_notation_for_strings.py
@@ -0,0 +1,10 @@
+song_title = 'Space Oddity'
+lyrics = "This is ground control to Major Tom / You've really made the grade"
+
+first_letter = song_title[0]
+seventh_letter = song_title[6]
+last_letter = lyrics[-1]
+
+print(first_letter)
+print(seventh_letter)
+print(last_letter)
