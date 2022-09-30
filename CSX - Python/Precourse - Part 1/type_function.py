ab5908d06c3fe52498253357acb0ae18e39a6b0d
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 10:57:46 -0500

    Add type_function.py

---
+++ CSX - Python/Precourse - Part 1/type_function.py
@@ -0,0 +1,11 @@
+fav_movie = 'Star Wars: Episode IV'
+times_seen = 732
+going_to_watch_again = True
+
+fav_movie_type = type(fav_movie)
+times_seen_type = type(times_seen)
+see_again_type = type(going_to_watch_again)
+
+print(fav_movie_type)
+print(times_seen_type)
+print(see_again_type)
