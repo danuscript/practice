af1c12ee8554dc27a5c5f636707e5489de10bf56
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 14:41:00 -0500

    Add were_awesome.py

---
+++ CSX - Python/Functions and Execution Context/were_awesome.py
@@ -0,0 +1,7 @@
+def were_awesome(str1, str2):
+    return f'{str2} and {str1} are awesome!'
+
+print(were_awesome('Dave', 'Will'))
+print(were_awesome('Victoria', 'Jenny'))
+print(were_awesome('Chris', 'Jac'))
+print(were_awesome('Phillip', 'Skyler'))
