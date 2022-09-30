e1ef84cedec37890588a3c5266eff6b11100bf0d
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 16:42:07 -0500

    Add add_waldo.py

---
+++ CSX - Python/Functions and Execution Context/add_waldo.py
@@ -0,0 +1,6 @@
+def add_waldo(names):
+    names['Waldo'] = 'unknown'
+    return names
+
+silicon_valley = { 'Richard': 'Hendrics', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle' }
+print(add_waldo(silicon_valley))
