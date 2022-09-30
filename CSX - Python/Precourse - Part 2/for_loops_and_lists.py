6fcf0d2d05d1ca2889d216f7df4d1b72cfef29bd
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 11:38:52 -0500

    Add for_loops_and_lists.py

---
+++ CSX - Python/Precourse - Part 2/for_loops_and_lists.py
@@ -0,0 +1,8 @@
+synonyms = ['fantastic', 'wonderful', 'great']
+greetings = []
+
+for synonym in synonyms:
+    greetings.append(f'Have a {synonym} day!')
+
+for greeting in greetings:
+    print(greeting)
