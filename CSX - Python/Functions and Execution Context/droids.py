1164c8683cc62eedbc8772aa20aee401cee494b3
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 15:33:47 -0500

    Add droids.py

---
+++ CSX - Python/Functions and Execution Context/droids.py
@@ -0,0 +1,11 @@
+def droids(arr):
+    for string in arr:
+        if string == 'Droids':
+            return 'Found Droids!'
+    return "These are not the droids you're looking for."
+
+star_wars = ['Luke', 'Finn', 'Rey', 'Kylo', 'Droids']
+thrones = ['Jon', 'Danny', 'Tyrion', 'The Mountain', 'Cersei']
+
+print(droids(star_wars))
+print(droids(thrones))
