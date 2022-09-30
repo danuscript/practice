d39b503cc3b90268828441f36b84dd311377e516
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 13:35:39 -0500

    Add control_flow_and_iteration.py

---
+++ CSX - Python/Precourse - Part 2/control_flow_and_iteration.py
@@ -0,0 +1,7 @@
+time_ten_if_over_five = [23, 9, 11, 2, 10, 6]
+
+for i, v in enumerate(time_ten_if_over_five):
+    if v > 5:
+        time_ten_if_over_five[i] = v * 10
+
+print(time_ten_if_over_five)
