4208109e5303b57fe7000502b74b1e47060ab78f
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 10:35:13 -0500

    Add numbers_exponents_and_remainders.py

---
+++ CSX - Python/Precourse - Part 1/numbers_exponents_and_remainders.py
@@ -0,0 +1,11 @@
+five_squared = 5 ** 2
+eight_cubed = 8 ** 3
+seven_to_the_sixth = 7 ** 6
+eighty_three_mod_six = 83 % 6
+sixty_mod_nine = 60 % 9
+
+print(five_squared)
+print(eight_cubed)
+print(seven_to_the_sixth)
+print(eighty_three_mod_six)
+print(sixty_mod_nine)
