b9e6021f1dcaab48ade43f91e729fd50c98fd65d
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 09:43:53 -0500

    Add primitive_data_types.py

---
+++ CSX - Python/Precourse - Part 1/primitive_data_types.py
@@ -0,0 +1,9 @@
+name = 'Danny'
+age = 24
+love_python = True
+list_of_complaints = None
+
+print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'It\'s {love_python}, I love Python')
+print(f'The list of things I hate about Python is {list_of_complaints}')
