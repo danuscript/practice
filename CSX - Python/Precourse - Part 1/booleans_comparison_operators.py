4018b12fb1a491743a9662ff865f861f117231ee
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 10:51:40 -0500

    Add booleans_comparison_operators.py

---
+++ CSX - Python/Precourse - Part 1/booleans_comparison_operators.py
@@ -0,0 +1,21 @@
+small = 2
+large = 5342
+
+is_smaller = (small < large)
+
+num = 45
+string = '45'
+
+is_equal = (num == string)
+
+is_true = True
+is_false = False
+
+is_true_not_false = (is_true != is_false)
+
+print('Is 2 < 5342?')
+print(is_smaller)
+print('Is 45 equal to "45"?')
+print(is_equal)
+print('Is true not equal to false?')
+print(is_true_not_false)
