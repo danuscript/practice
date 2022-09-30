c34bd9f1e2d345e7dfd51081d4fb9dca33dfc645
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-09-30 16:10:00 -0500

    Add while_loops_counters.py

---
+++ CSX - Python/Functions and Execution Context/while_loops_counters.py
@@ -0,0 +1,12 @@
+def im_about_to_explode_with_excitement(n):
+    while n > 0:
+        if n == 5:
+            print("Oh wow, I can't handle the anticipation!")
+        elif n == 3:
+            print("I'm about to explode with excitement!")
+        else:
+            print(n)
+        n -= 1
+    print('That was kind of a let down')
+
+im_about_to_explode_with_excitement(10
