28fa472d3e461e3b07f95b95e68dac5ad612903f
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-10-01 09:40:16 -0500

    Add group_by.py

---
+++ CSX - Python/Callbacks & Higher-order Functions/group_by.py
@@ -0,0 +1,18 @@
+from math import floor 
+
+def group_by(ls, cb):
+    out = {}
+    for x in ls:
+        res = cb(x)
+        if res in out:
+            out[res].append(x)
+        else:
+            out[res] = [x]
+    return out
+
+decimals = [1.3, 2.1, 2.4]
+
+def floored(n):
+    return floor(n)
+
+print(group_by(decimals, floored))
