917a53114f529aff9ceb5fb8cbab5eb0ef3f54cb
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-10-01 11:11:42 -0500

    Add cycle_iterator.py

---
+++ CSX - Python/Closure, Scope & Execution Context/cycle_iterator.py
@@ -0,0 +1,18 @@
+def cycle_iterator(ls):
+    i = -1
+    def inner():
+        nonlocal i
+        i += 1
+        if i >= len(ls):
+            i = 0
+            return ls[i]
+        return ls[i]
+    return inner
+
+three_day_weekend = ['Fri', 'Sat', 'Sun']
+get_day = cycle_iterator(three_day_weekend)
+
+print(get_day())
+print(get_day())
+print(get_day())
+print(get_day())
