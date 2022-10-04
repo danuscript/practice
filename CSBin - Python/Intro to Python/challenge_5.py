fe015d00441754b880bdcbac72a1cd86b4ed1e38
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-10-04 12:20:15 -0500

    Add challenge_5.py

---
+++ CSBin - Python/Intro to Python/challenge_5.py
@@ -0,0 +1,4 @@
+print('CodeSmith'.index('o') == 1) # -> True
+print('hello'.index('ll') == 2) # -> True
+print('zebra'.index('z') == 0) # -> True
+# print('banana'.index('B')) # -> ValueError: substring not found
