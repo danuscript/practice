5dffa2886069fa1b15d07c909cd9811390730d76
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-10-04 11:20:25 -0500

    Add challenge_1.py

---
+++ CSBin - Python/Intro to Python/challenge_1.py
@@ -0,0 +1,9 @@
+print(isinstance(7.0, float)) # True
+print(isinstance(22, int)) # True
+print(isinstance(True, bool)) # True
+
+my_var = None
+print(my_var is None) # True
+
+my_var = 'Danny'
+print(isinstance(my_var, str)) # True
