447c1c0fa3b9c49ece3ccfa20d866b9631c02835
Danny Wallace <108024160+danuscript@users.noreply.github.com>
2022-10-04 20:05:13 -0500

    Update challenge_12.py

--- CSBin - Python/Callbacks/challenge_12.py
+++ CSBin - Python/Callbacks/challenge_12.py
@@ -1,5 +1,5 @@
 def majority(ls, cb):
-    return len([x for x in ls if cb(x)]) > len(ls) / 2
+    return sum(1 for x in ls if cb(x)) > len(ls) / 2
 
 print(majority([1, 2, 3, 4, 5], lambda x: x % 2 == 1))
 print(majority([2, 3, 4, 5], lambda x: x % 2 == 1))
